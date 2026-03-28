c2b0094e3b7dac0500c08fecc88ef3f4ecf59414
priyanshu0009 <smonu5366@gmail.com>
2026-03-28 15:04:09 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = "https://bug-tracker-system-2mjo.onrender.com/api";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
